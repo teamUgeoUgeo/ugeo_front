7f3f2426c39f145ae23327a369b3d4cc33723b7b
pearl-sea <sakanaplanet@gmail.com>
2023-05-21 17:31:05 +0900

    [Feat] 1시간뒤 자동 로그아웃 구현

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,7 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 import Header from "../components/Header";
+import { getTokenDuration } from "../util/auth";
 
 const RootLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/user/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
   return (
     <>
       <Header />

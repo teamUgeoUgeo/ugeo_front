17dbb9cf24515b8a19262815b9b387e7b3060619
pearl-sea <sakanaplanet@gmail.com>
2023-05-21 18:16:37 +0900

    [Feat] 글쓰기 기능 개선

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,26 @@
-import { Link, useRouteLoaderData, useActionData, redirect } from "react-router-dom";
-import { useEffect, useState } from "react";
-import PageContent from "../components/PageContent";
-import Sidebar from "../components/Sidebar";
-import PostForm from "../components/PostForm";
-import PostList from "../components/PostList";
-import classes from "../components/PageContent.module.css";
+import {
+  Link,
+  useRouteLoaderData,
+  useActionData,
+  redirect,
+} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import PageContent from '../components/PageContent';
+import Sidebar from '../components/Sidebar';
+import PostForm from '../components/PostForm';
+import PostList from '../components/PostList';
+import classes from '../components/PageContent.module.css';
 
 const HomePage = () => {
   const newData = useActionData();
-  const token = useRouteLoaderData("root");
+  const token = useRouteLoaderData('root');
   const [data, setData] = useState(null);
 
   const fetchData = async (token) => {
-    const response = await fetch("/api/article/", {
+    const response = await fetch('/api/article/', {
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
     const data = await response.json();
@@ -28,7 +33,6 @@ const HomePage = () => {
     } else {
       setData(null);
     }
-
   }, [token, newData]);
 
   if (!data) {
@@ -37,7 +41,7 @@ const HomePage = () => {
         <h1>메인화면</h1>
         <p>로그인 이전의 메인화면입니다.</p>
         <p>로그인을 해 주세요.</p>
-        <Link className={classes.link + " link"} to="/user/login">
+        <Link className={classes.link + ' link'} to="/user/login">
           로그인하기
         </Link>
       </PageContent>
@@ -58,32 +62,30 @@ const HomePage = () => {
 export default HomePage;
 
 export async function action({ request }) {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   const data = await request.formData();
   const postData = {
-    amount: data.get("amount"),
-    detail: data.get("detail"),
+    amount: data.get('amount'),
+    detail: data.get('detail'),
   };
 
   let header = {
     Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   };
 
   let body = JSON.stringify(postData);
 
-  const response = await fetch("/api/article/", {
-    method: "POST",
+  const response = await fetch('/api/article/', {
+    method: 'POST',
     headers: header,
     body: body,
   });
 
   if (response.ok) {
     return true;
-  } else {
-    console.log(response)
   }
 
-  return redirect("/");
+  return redirect('/');
 }

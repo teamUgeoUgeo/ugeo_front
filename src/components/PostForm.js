b97a8ca9621e595edab375f8df6fbea73985cd09
pearl-sea <sakanaplanet@gmail.com>
2023-05-30 00:21:07 +0900

    [Feat] 글 수정, 삭제 구현

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,63 @@
-import React from "react";
-import { Form } from "react-router-dom";
+import PropTypes from "prop-types";
+import React, { useRef, useState } from "react";
 import classes from "./PostForm.module.css";
 
-const PostForm = () => {
+const PostForm = ({ onSubmit }) => {
+  const [amount, setAmount] = useState("");
+  const [detail, setDetail] = useState("");
+
+  const formRef = useRef();
+
+  const onSubmitHandler = () => {
+    const postData = {
+      amount: amount,
+      detail: detail,
+    };
+
+    const method = "POST";
+
+    onSubmit(postData, method);
+    formRef.current.reset();
+  };
+
+  const onChangeAmountHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const onChangeDetailHandler = (event) => {
+    setDetail(event.target.value);
+  };
+
+  const checkValue = amount !== "" && detail !== "";
+
   return (
-    <Form className={classes.form} method="post">
-      <input type="text" name="amount" placeholder="소비한 금액" />
-      <textarea name="detail" id="" placeholder="어디다 썼나요?"></textarea>
-      <button className={`${classes.button} default`}>등록하기</button>
-    </Form>
+    <form ref={formRef} className={classes.form} method="post">
+      <input
+        type="number"
+        name="amount"
+        placeholder="소비한 금액"
+        onChange={onChangeAmountHandler}
+      />
+      <textarea
+        name="detail"
+        placeholder="어디다 썼나요?"
+        maxLength={255}
+        onChange={onChangeDetailHandler}
+      ></textarea>
+      <button
+        disabled={checkValue ? false : true}
+        type="button"
+        className={`${classes.button} ${checkValue ? "" : `disabled`} default`}
+        onClick={onSubmitHandler}
+      >
+        등록하기
+      </button>
+    </form>
   );
+};
+
+PostForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default PostForm;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,29 +3,59 @@ import React, { useState, useRef, useEffect } from "react";
 import classes from "./PostList.module.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const PostList = (props) => {
-  const [showEdit, setShowEdit] = useState([]);
-  const editRef = useRef(null);
+const PostList = ({ datas, onDelete, onModify }) => {
+  const [showEdit, setShowEdit] = useState("");
+  const [toggleModify, setToggleModify] = useState(null);
+  const [changeAmount, setChangeAmount] = useState(null);
+  const [changeDetail, setChangeDetail] = useState(null);
+  const editRef = useRef();
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (editRef.current && !editRef.current.contains(event.target)) {
-        setShowEdit([]);
-      }
+  const onShowEdithandler = (id) => {
+    setShowEdit(id);
+  };
+
+  const onModifyHandler = (data) => {
+    setToggleModify(data.id);
+    setChangeAmount(data.amount);
+    setChangeDetail(data.detail);
+  };
+
+  const onChangeAmount = (event) => {
+    setChangeAmount(event.target.value);
+  };
+
+  const onChangeDetail = (event) => {
+    setChangeDetail(event.target.value);
+  };
+
+  const onCancelModifyHandler = () => {
+    setToggleModify("");
+    setShowEdit("");
+  };
+
+  const onCompleteModifyHandler = (data) => {
+    const postData = {
+      amount: changeAmount,
+      detail: changeDetail,
+      article_id: data.id,
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [editRef]);
+    const method = "PUT";
 
-  const onClickhandler = (index) => {
-    setShowEdit((showEdit) => {
-      const copy = [...showEdit];
-      copy[index] = true;
-      return copy;
-    });
+    onModify(postData, method);
+    setToggleModify(null);
+    setShowEdit(null);
+
+    if (postData.detail === "" || postData.amount === "") {
+      return;
+    }
+
+    data.amount = changeAmount;
+    data.detail = changeDetail;
+  };
+
+  const onDeleteHandler = (id) => {
+    onDelete(id);
   };
 
   const decoAmount = (amount) => {
@@ -50,35 +80,95 @@ const PostList = (props) => {
     return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (editRef.current && !editRef.current.contains(event.target)) {
+        setShowEdit(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [editRef]);
+
   return (
     <ul className={classes.list}>
-      {props.datas.map((data, index) => {
+      {datas.map((data) => {
         return (
           <li key={data.id}>
-            <article className={classes.top}>
-              <span className={`${classes.amount} ${decoAmount(data.amount)}`}>
-                {addComma(data.amount)}
-              </span>
-              <div
-                className={classes.more}
-                onClick={() => onClickhandler(index)}
-              >
-                <MoreVertIcon />
-              </div>
-              {showEdit[index] && (
-                <ul ref={editRef} className={classes.edit}>
-                  <li>
-                    <button>수정</button>
-                  </li>
-                  <li>
-                    <button>삭제</button>
-                  </li>
-                </ul>
-              )}
-            </article>
-            <div className={classes.content}>
-              <p className={classes.detail}>{data.detail}</p>
-            </div>
+            {toggleModify !== data.id && (
+              <article className={classes.article}>
+                <div className={classes.top}>
+                  <div className={classes.userinfo}>
+                    <span
+                      className={`${classes.amount} ${decoAmount(data.amount)}`}
+                    >
+                      {addComma(data.amount)}
+                    </span>
+                    {/* <span className={classes.nickname}>{data.nickname}</span>
+                    <span className={classes.username}>@{data.username}</span> */}
+                  </div>
+                  <div
+                    className="icon"
+                    onClick={() => onShowEdithandler(data.id)}
+                  >
+                    <MoreVertIcon />
+                  </div>
+                  {showEdit === data.id && (
+                    <ul ref={editRef} className={classes.edit}>
+                      <li>
+                        <button onClick={() => onModifyHandler(data)}>
+                          수정
+                        </button>
+                      </li>
+                      <li>
+                        <button onClick={() => onDeleteHandler(data.id)}>
+                          삭제
+                        </button>
+                      </li>
+                    </ul>
+                  )}
+                </div>
+                <div className={classes.content}>
+                  <p className={classes.detail}>{data.detail}</p>
+                </div>
+              </article>
+            )}
+            {toggleModify === data.id && (
+              <form className={classes.form}>
+                <input
+                  className={classes.changeAmount}
+                  type="text"
+                  name="amount"
+                  defaultValue={data.amount}
+                  onChange={onChangeAmount}
+                />
+                <textarea
+                  name="detail"
+                  maxLength={255}
+                  defaultValue={data.detail}
+                  onChange={onChangeDetail}
+                ></textarea>
+                <div className="flex-row">
+                  <button
+                    type="button"
+                    className={`${classes.modify} default`}
+                    onClick={() => onCompleteModifyHandler(data)}
+                  >
+                    수정
+                  </button>
+                  <button
+                    type="button"
+                    className={`${classes.cancel} default`}
+                    onClick={onCancelModifyHandler}
+                  >
+                    취소
+                  </button>
+                </div>
+              </form>
+            )}
           </li>
         );
       })}
@@ -88,6 +178,8 @@ const PostList = (props) => {
 
 PostList.propTypes = {
   datas: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onModify: PropTypes.func.isRequired,
 };
 
 export default PostList;

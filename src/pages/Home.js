bde8eb19d6b1a84935d178713145edabb48fd265
pearl-sea <sakanaplanet@gmail.com>
2023-05-30 00:21:07 +0900

    [Feat] 글 수정, 삭제 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,5 @@
-import React from "react";
-import {
-  Link,
-  useRouteLoaderData,
-  useActionData,
-  redirect,
-} from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useRouteLoaderData, useNavigate } from "react-router-dom";
 import PageContent from "../components/PageContent";
 import Sidebar from "../components/Sidebar";
 import PostForm from "../components/PostForm";
@@ -13,9 +7,9 @@ import PostList from "../components/PostList";
 import classes from "../components/PageContent.module.css";
 
 const HomePage = () => {
-  const newData = useActionData();
   const token = useRouteLoaderData("root");
   const [data, setData] = useState(null);
+  const navigate = useNavigate();
 
   const fetchData = async (token) => {
     const response = await fetch("/api/article/", {
@@ -24,70 +18,88 @@ const HomePage = () => {
         "Content-Type": "application/json",
       },
     });
-    const data = await response.json();
-    setData(data);
+    const getResdata = await response.json();
+    setData(getResdata);
+
+    if (!response.ok) {
+      navigate("/error");
+    }
   };
 
   useEffect(() => {
-    if (token || newData) {
+    if (token) {
       fetchData(token);
     } else {
       setData(null);
     }
-  }, [token, newData]);
+  }, [token]);
+
+  const handleDelete = async (id) => {
+    setData(data.filter((el) => el.id !== id));
+
+    const response = await fetch(`/api/article/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      navigate("/error");
+    }
+  };
+
+  const handleSubmit = async (postData, method) => {
+    let header = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+
+    const response = await fetch("/api/article/", {
+      method: method,
+      headers: header,
+      body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+      navigate("/error");
+    }
+
+    if (method === "POST") {
+      const postResData = await response.json();
+      postData["id"] = postResData.article_id;
+      setData([postData, ...data]);
+    }
+  };
 
   return (
     <PageContent>
-      {!token && (
-        <>
+      {!token && !data && (
+        <div className={`${classes.logout} max-width`}>
           <h1>메인화면</h1>
           <p>로그인 이전의 메인화면입니다.</p>
           <p>로그인을 해 주세요.</p>
           <Link className={classes.link + " link"} to="/user/login">
             로그인하기
           </Link>
-        </>
+        </div>
       )}
-      {data && (
-        <>
+      {token && data && (
+        <div className={`${classes.login} max-width`}>
           <Sidebar />
           <section className={classes.section}>
-            <PostForm />
-            <PostList datas={data} />
+            <PostForm onSubmit={handleSubmit} />
+            <PostList
+              datas={data}
+              onDelete={handleDelete}
+              onModify={handleSubmit}
+            />
           </section>
-        </>
+        </div>
       )}
     </PageContent>
   );
 };
 
 export default HomePage;
-
-export async function action({ request }) {
-  const token = localStorage.getItem("token");
-
-  const data = await request.formData();
-  const postData = {
-    amount: data.get("amount"),
-    detail: data.get("detail"),
-  };
-
-  let header = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
-
-  let body = JSON.stringify(postData);
-
-  const response = await fetch("/api/article/", {
-    method: "POST",
-    headers: header,
-    body: body,
-  });
-
-  if (response.ok) {
-    return true;
-  }
-
-  return redirect("/");
-}

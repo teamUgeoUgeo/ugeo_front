18c82ce3dd55fccfd4be46c567107572f18b8d15
pearl-sea <sakanaplanet@gmail.com>
2023-05-30 11:27:04 +0900

    [Feat] 프록시 타겟 변경

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 export default function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://ugeo-back.sigae.kim",
+      target: "https://ugeo.sigae.kim",
       changeOrigin: true,
     })
   );

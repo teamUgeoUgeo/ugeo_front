88171872d45f60f3b49729286bd72a7b6ac3a7ca
pearl-sea <sakanaplanet@gmail.com>
2023-05-30 11:17:18 +0900

    [Style] 누락된 css 클래스 추가

--- src/pages/CreateComplete.js
+++ src/pages/CreateComplete.js
@@ -6,14 +6,14 @@ import classes from "../components/PageContent.module.css";
 const CreateCompletePage = () => {
   return (
     <PageContent>
-      <>
+      <div className={`${classes.logout} max-width`}>
         <h1>회원가입 완료!</h1>
         <p>회원이 되셨습니다.</p>
         <p>로그인을 해 주세요.</p>
         <Link className={classes.link + " link"} to="/user/login">
           로그인하기
         </Link>
-      </>
+      </div>
     </PageContent>
   );
 };

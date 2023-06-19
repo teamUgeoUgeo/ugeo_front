f7f8e369b39ed315ab587d01fdd6ecefe4b56e16
pearl-sea <sakanaplanet@gmail.com>
2023-06-19 23:21:25 +0900

    [!HOTFIX] 웹소켓 오류 수정

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,42 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
   },
-  target: "web",
+  target: 'web',
   devServer: {
-    port: "3000",
+    port: '3000',
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, 'dist'),
     },
     allowedHosts: 'all',
     historyApiFallback: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".css"],
+    extensions: ['.js', '.jsx', '.json', '.css'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-      scriptType: "text/javascript",
+      template: path.join(__dirname, 'public', 'index.html'),
+      scriptType: 'text/javascript',
     }),
   ],
 };

5cf9c7267985175c36ca662f88846ddf249ec3d9
pearl-sea <sakanaplanet@gmail.com>
2023-07-31 15:49:29 +0900

    [Feat] 문자열 상수로 관리

---
+++ src/constants/dataTypes.js
@@ -0,0 +1,14 @@
+export const DATA_TYPE = Object.freeze({
+  email: "email",
+  username: "username",
+  nickname: "nickname",
+  password: "password",
+  confirmPassword: "confirm-password",
+});
+
+export const INPUT_TYPE = Object.freeze({
+  text: "text",
+  password: "password",
+  button: "button",
+  submit: "submit",
+});

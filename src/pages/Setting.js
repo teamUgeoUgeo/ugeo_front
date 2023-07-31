50c760b4b9deeb2d67037682aed51b9c5af277b4
pearl-sea <sakanaplanet@gmail.com>
2023-07-31 16:10:58 +0900

    [Feat] 회원 정보 수정 페이지 구현

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import DropBox from "../components/DropBox";
 import PageContent from "../components/PageContent";
 import classes from "../components/PageContent.module.css";
+import SettingUserForm from "../components/SettingUserForm";
 import Sidebar from "../components/Sidebar";
 import { getAuthToken } from "../util/auth";
+
+export const SETTING_TITLE = {
+  userinfo: "회원 정보 변경",
+  password: "비밀번호 변경",
+};
 
 const SettingPage = () => {
   const token = getAuthToken();
@@ -15,16 +22,14 @@ const SettingPage = () => {
           <div className={`${classes.login} max-width`}>
             <Sidebar></Sidebar>
             <section className={classes.section}>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">탈퇴</Link>
-                  </li>
-                  <li>
-                    <Link to="/">정보 변경</Link>
-                  </li>
-                </ul>
-              </nav>
+              <DropBox
+                title={SETTING_TITLE.userinfo}
+                children={<SettingUserForm title={SETTING_TITLE.userinfo} />}
+              />
+              <DropBox
+                title={SETTING_TITLE.password}
+                children={<SettingUserForm title={SETTING_TITLE.password} />}
+              />
             </section>
           </div>
         </PageContent>
